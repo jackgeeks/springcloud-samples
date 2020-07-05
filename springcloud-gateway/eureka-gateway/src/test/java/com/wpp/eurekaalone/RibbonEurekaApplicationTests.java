be9b6de84d44949129c54bdd2afebe886d493b8c
jackgreek <584847699@qq.com>
2020-07-05 22:57:01 +0800

    《第五章第二小节:服务网关之Gateway》

---
+++ springcloud-gateway/eureka-gateway/src/test/java/com/wpp/eurekaalone/RibbonEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpp.eurekaalone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
